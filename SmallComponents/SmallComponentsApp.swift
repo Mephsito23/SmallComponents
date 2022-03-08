170f23ef49e7f0cc940e9c5a9c6e92a206e5c204
Mephsito <xiezefu@outlook.com>
2022-03-08 14:49:18 +0800

    Initial Commit

---
+++ SmallComponents/SmallComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallComponentsApp.swift
+//  SmallComponents
+//
+//  Created by Mephisto on 2022/3/8.
+//
+
+import SwiftUI
+
+@main
+struct SmallComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
